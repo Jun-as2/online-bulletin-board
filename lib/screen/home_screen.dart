e0023b62bee532b9b0aa5d790d3e9430661b8f3c
swissmissed <shkim7020@naver.com>
2024-06-12 00:29:48 +0900

    홈 화면 앱바 구성 및, 로그아웃 기능 구현, 게시글 추가 화면 생성

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,8 +1,10 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:online_bulletin_board/screen/add_post_screen.dart';
+import 'package:online_bulletin_board/screen/sign_in_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
-
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
@@ -10,6 +12,48 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: customAppBar(),
+    );
+  }
+
+  AppBar customAppBar() {
+    return AppBar(
+      backgroundColor: Colors.black12,
+      leading: IconButton(
+        icon: const Icon(Icons.logout_outlined),
+        onPressed: () {
+          FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+          firebaseAuth.signOut();
+          Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(
+                builder: (context) => SignInScreen(),
+              ));
+        },
+      ),
+      title: const Text(
+        'Community',
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 24,
+        ),
+      ),
+      actions: [
+        IconButton(
+            onPressed: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => AddPostScreen(),
+                  )).then(
+                (value) {
+                  setState(() {});
+                },
+              );
+            },
+            icon: Icon(Icons.add)),
+      ],
+    );
   }
 }
