9228de68192f19545ba1ac256d2229378754e008
cksdid202 <lcy211445@gmail.com>
2024-06-12 00:08:55 +0900

    로그인 화면 구성과 로그인 기능 완료 + 회원가입 화면 생성

--- lib/screen/sign_in_screen.dart
+++ lib/screen/sign_in_screen.dart
@@ -1,4 +1,8 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:online_bulletin_board/screen/home_screen.dart';
+import 'package:online_bulletin_board/screen/sign_up_screen.dart';
 
 class SignInScreen extends StatefulWidget {
   const SignInScreen({super.key});
@@ -8,8 +12,166 @@ class SignInScreen extends StatefulWidget {
 }
 
 class _SignInScreenState extends State<SignInScreen> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+
+  Future<void> _signIn() async {
+    try {
+      var userCredential = await firebaseAuth.signInWithEmailAndPassword(
+          email: emailController.text, password: passwordController.text);
+      Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) => HomeScreen(),
+          ));
+    } on FirebaseAuthException catch (error) {
+      print(error.code);
+      switch (error.code) {
+        case "invalid-email":
+          Fluttertoast.showToast(msg: "유효하지 않은 이메일입니다");
+          break;
+        case "user-disabled":
+          Fluttertoast.showToast(msg: "비활성화된 사용자입니다");
+          break;
+        case "user-not-found":
+          Fluttertoast.showToast(msg: "등록되지 않은 사용자입니다");
+          break;
+        case "wrong-password":
+          Fluttertoast.showToast(msg: "비밀번호가 틀립니다");
+          break;
+        default:
+          Fluttertoast.showToast(msg: "로그인에 실패하였습니다");
+          break;
+      }
+      return;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Form(
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.all(30.0),
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 150,
+              ),
+              const Text(
+                'Welcome',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 50,
+                    fontWeight: FontWeight.w800),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              const Text(
+                'Login to your account',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 25,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+              const SizedBox(
+                height: 100,
+              ),
+              TextFormField(
+                keyboardType: TextInputType.name,
+                controller: emailController,
+                decoration: InputDecoration(
+                  prefixIcon: const Icon(Icons.person_outline),
+                  labelText: 'email',
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0)),
+                ),
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              TextFormField(
+                keyboardType: TextInputType.visiblePassword,
+                controller: passwordController,
+                obscureText: true,
+                decoration: InputDecoration(
+                  prefixIcon: const Icon(Icons.lock_outline),
+                  labelText: 'password',
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0)),
+                ),
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              ElevatedButton(
+                onPressed: () async {
+                  await _signIn();
+                  User? user = firebaseAuth.currentUser;
+                  if (user == null) {
+                    return;
+                  } else {
+                    Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => HomeScreen(),
+                        ));
+                  }
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.white,
+                  minimumSize: const Size.fromHeight(50.0),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                ),
+                child: const Text(
+                  'Login',
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 15,
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    'Don\'t have an account?',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const SignUpScreen(),
+                          )).then(
+                        (value) {
+                          setState(() {});
+                        },
+                      );
+                    },
+                    child: Text(
+                      'Sign Up',
+                      style: TextStyle(
+                        color: Colors.indigo[200],
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
