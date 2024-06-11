d2803c75616b26b9ee137478bee118890152ff1a
Jun-as2 <wns4017@daum.net>
2024-06-11 23:11:20 +0900

    SDK 설정 변경 및 테마 추가.

--- lib/main.dart
+++ lib/main.dart
@@ -4,19 +4,26 @@ import 'package:flutter/material.dart';
 import 'package:online_bulletin_board/firebase_options.dart';
 import 'package:online_bulletin_board/screen/home_screen.dart';
 import 'package:online_bulletin_board/screen/sign_in_screen.dart';
+import 'package:online_bulletin_board/theme.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
-  runApp(MainApp());
+  runApp(MainApp(
+    appTheme: AppTheme(),
+  ));
 }
 
 class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+  const MainApp({super.key, required this.appTheme});
+
+  final AppTheme appTheme;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: appTheme.dark,
       home: StreamBuilder<User?>(
         builder: (context, userSnapshot) {
           if (userSnapshot.hasData) {
---
+++ lib/theme.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+abstract class AppColors {
+  static const secondary = Color(0xFF3B76F6);
+  static const accent = Color(0xFFD6755B);
+  static const textDark = Color(0xFF53585A);
+  static const textLigth = Color(0xFFF5F5F5);
+  static const textFaded = Color(0xFF9899A5);
+  static const iconLight = Color(0xFFB1B4C0);
+  static const iconDark = Color(0xFFB1B3C1);
+  static const textHighlight = secondary;
+  static const cardLight = Color(0xFFF9FAFE);
+  static const cardDark = Color(0xFF303334);
+}
+
+abstract class _DarkColors {
+  static const background = Color(0xFF1B1E1F);
+  static const card = AppColors.cardDark;
+}
+
+class AppTheme {
+  static const accentColor = AppColors.accent;
+  static final visualDensity = VisualDensity.adaptivePlatformDensity;
+
+  final darkBase = ThemeData.dark();
+
+  /// Dark theme and its settings.
+  ThemeData get dark => ThemeData(
+        brightness: Brightness.dark,
+        colorScheme: darkBase.colorScheme.copyWith(secondary: accentColor),
+        visualDensity: visualDensity,
+        textTheme:
+            GoogleFonts.interTextTheme().apply(bodyColor: AppColors.textLigth),
+        appBarTheme: darkBase.appBarTheme.copyWith(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          centerTitle: true,
+          titleTextStyle: const TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 17,
+          ),
+          systemOverlayStyle: SystemUiOverlayStyle.light,
+        ),
+        primaryColor: _DarkColors.background,
+        scaffoldBackgroundColor: _DarkColors.background,
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(foregroundColor: AppColors.secondary),
+        ),
+        cardColor: _DarkColors.card,
+        primaryTextTheme: const TextTheme(
+          // ignore: deprecated_member_use
+          headlineLarge: TextStyle(color: AppColors.textLigth),
+        ),
+        iconTheme: const IconThemeData(color: AppColors.iconLight),
+      );
+}
